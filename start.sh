03416aa44d3d10a2a85b49c3c6c4fd4acf69ece6
Hamza-Shakeel1 <hamzashakeel219@gmail.com>
2026-01-08 19:36:33 +0500

    Fix start command

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Install dependencies in root
-pip install --upgrade pip
+
 pip install -r requirements.txt
 
-# Start FastAPI app
 python -m uvicorn backend.main:app --host 0.0.0.0 --port ${PORT:-8000}
