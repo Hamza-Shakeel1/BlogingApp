c30c5d289377eda6868be78f40f5893aeb450df1
Hamza-Shakeel1 <hamzashakeel219@gmail.com>
2026-01-09 17:08:11 +0500

    Update f

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./UserProfile.css"; // updated CSS file name
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "GET https://blogingapp-production.up.railway.app"
+
 
 const UserProfile = () => {
   const [loading, setLoading] = useState(true);
