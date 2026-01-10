ca3b6151de5b5bb4dac32117c1349445f0720b41
Hamza-Shakeel1 <hamzashakeel219@gmail.com>
2026-01-10 19:23:13 +0500

    Update pages

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -5,45 +5,46 @@ const Sidebar = ({ auth }) => {
   const user = auth?.user;
 
   return (
-    <div className="sidebar">
-      <NavLink
-        to="/user-profile"
-        className={({ isActive }) =>
-          isActive ? "sidebar-link active" : "sidebar-link"
-        }
-      >
-        User Profile
-      </NavLink>
+    <div ></div>
+    // <div className="sidebar">
+    //   <NavLink
+    //     to="/user-profile"
+    //     className={({ isActive }) =>
+    //       isActive ? "sidebar-link active" : "sidebar-link"
+    //     }
+    //   >
+    //     User Profile
+    //   </NavLink>
 
-      <NavLink
-        to="/posts"
-        className={({ isActive }) =>
-          isActive ? "sidebar-link active" : "sidebar-link"
-        }
-      >
-        All Posts
-      </NavLink>
+    //   <NavLink
+    //     to="/posts"
+    //     className={({ isActive }) =>
+    //       isActive ? "sidebar-link active" : "sidebar-link"
+    //     }
+    //   >
+    //     All Posts
+    //   </NavLink>
 
-      <NavLink
-        to="/my-posts"
-        className={({ isActive }) =>
-          isActive ? "sidebar-link active" : "sidebar-link"
-        }
-      >
-        My Posts
-      </NavLink>
+    //   <NavLink
+    //     to="/my-posts"
+    //     className={({ isActive }) =>
+    //       isActive ? "sidebar-link active" : "sidebar-link"
+    //     }
+    //   >
+    //     My Posts
+    //   </NavLink>
 
-      {user?.role === "admin" && (
-        <NavLink
-          to="/create-post"
-          className={({ isActive }) =>
-            isActive ? "sidebar-link active" : "sidebar-link"
-          }
-        >
-          Admin Dashboard
-        </NavLink>
-      )}
-    </div>
+    //   {user?.role === "admin" && (
+    //     <NavLink
+    //       to="/create-post"
+    //       className={({ isActive }) =>
+    //         isActive ? "sidebar-link active" : "sidebar-link"
+    //       }
+    //     >
+    //       Admin Dashboard
+    //     </NavLink>
+    //   )}
+    // </div>
   );
 };
 
